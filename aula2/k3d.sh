2314ccbf0c8de4cbc3309ed8a65a93878c4a9d4d
abilioj <abilioj@live.com>
2023-01-25 02:25:19 -0300

    up - AULA 2 parte service

--- aula2/k3d.sh
+++ aula2/k3d.sh
@@ -5,7 +5,7 @@ k3d cluster create
 k3d cluster create --no-lb
 
 # cria um cluster com mais contropane e worknodes
-k3d cluster create nomedocluster --servers 3 --agents 3
+k3d cluster create nomedocluster --servers 2 --agents 2
 
 # lista
 k3d cluster list
@@ -13,3 +13,4 @@ k3d cluster list
 # delete
 k3d cluster delete
 
+# -----------------------------------------------------------------------
--- aula2/kubenetes.sh
+++ aula2/kubenetes.sh
@@ -1,14 +1,78 @@
 # kubenetes
 
-# lista cluster
+# lista tudo
+kubectl get all
+
+# lista os nó do cluster
 kubectl get nodes
 # container do cluster
 docker container ls
 
-# cria o cluster local: necessario o kubectl e o k3d
+# lista todos os objetos do cluster kubenetes a ser usado pele pod nas tags
+kubectl api-resources
 
-kubectl delete pod meupod
+# -----------------------------------------------------------------------
+# -- pods
 
-kubectl delete pod meupod-azul
+    # cria o cluster local com arquivo pod.yaml: necessario o kubectl e o k3d
 
-kubectl apply -f .\pod.yaml
+    kubectl delete pod meupod
+
+    kubectl delete pod meupod-azul
+
+    # cria pod
+    kubectl create -f .\pod.yaml
+    # -- cria e faz atualizações no pod
+    kubectl apply -f .\pod.yaml
+
+    # deleta os pods atraveis do arquivo utilizado pra a criação "pod.yaml"
+    kubectl delete -f .\pod.yaml
+
+    # lista os pods
+    kubectl get pods
+
+    # ver mais infomação do pod "meupod"
+    kubectl describe pod meupod
+
+    # diponibilizando o acesso ao pod "meupod", redimeciondo a posta 80 pra 8080
+    kubectl port-forward pod/meupod 8080:80 
+
+    #  pesquisando pode por labels
+    kubectl get pods -l cor=azul
+
+# -----------------------------------------------------------------------
+# -- ReplicaSet
+
+    # cria
+    kubectl create -f .\replicaset.yaml
+    # -- cria e faz atualizações
+    kubectl apply -f .\replicaset.yaml
+
+    # lista o replicaset
+    kubectl get replicaset
+
+    # ver mais infomação do replicaset "meureplicaset"
+    kubectl describe replicaset meureplicaset
+
+    # deleta os pods atraveis do arquivo utilizado pra a criação
+    kubectl delete -f .\replicaset.yaml
+
+# -----------------------------------------------------------------------
+# -- Deployment
+
+# cria
+kubectl create -f .\deployment.yaml
+# -- cria e faz atualizações
+kubectl apply -f .\deployment.yaml
+
+# volta o deployment anterio do "meurdeployment"
+kubectl rollout undo deployment meurdeployment
+
+# -----------------------------------------------------------------------
+# -- service
+
+# lista
+kubectl get svc
+
+ # diponibilizando o acesso ao service "web-page", redimeciondo a posta 80 pra 8080
+kubectl port-forward service/web-page 8080:80
