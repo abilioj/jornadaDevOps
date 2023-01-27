1626c573d2ac54416fd9d80465e305ae718c101e
abilioj <abilioj@live.com>
2023-01-27 01:38:55 -0300

    up - AULA 3 - completa

--- aula3/INICIO DA AULA.sh
+++ aula3/INICIO DA AULA.sh
@@ -1,17 +1,14 @@
 # criar chave ssh
 ssh-keygen -t rsa -b 2048
 
-# loca e name - ~/.ssh/terraform
-
-# pwd
-ajf123123
+# local e name - ~/.ssh/terraform
 
 # acessa a vm
 ssh -i C:\Users\AJGF\.ssh\terraform root@
 ssh -i C:\Users\abilio.jose\.ssh\terraform root@
 
 # acessa o kubenete remoto
-# primero baixa o arquivo de cofiguração de acessa
+# primero baixa o arquivo de configuração de acessa
 # e seta no arquivo ~\.kube\config
 
 # em type da tag "kind: Service" difine 'LoadBalancer'
@@ -27,3 +24,14 @@ kubectl apply -f .\deployment.yaml
 # depois de feze as primero config rode o comando abraixo pra iniciaos providers
 terraform init
 
+# pra aplica a configuração no cloud
+terraform apply
+
+# formata o arquivo .tf
+terraform fmt 
+
+# mostra o plano de execução
+terraform plan
+
+# para destrui sua estruta na cloud
+terraform destroy
